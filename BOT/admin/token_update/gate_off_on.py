2c727c1099e71326cdd03b26de1e615d4b9a0d7b
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-20 15:51:28 -0400

    Update gate_off_on.py

--- BOT/admin/token_update/gate_off_on.py
+++ BOT/admin/token_update/gate_off_on.py
@@ -12,7 +12,7 @@ async def update_dead_amount(Client, message):
 
 Message: To Perform This Action, You Need Admin Level Power. 
 
-Contact @Reo_47 For More Info ✅</b>"""
+Contact @Eretro_7 For More Info ✅</b>"""
             await message.reply_text(resp)
             return
 
@@ -62,7 +62,7 @@ async def remove_command(Client, message):
 
 Message: To Perform This Action, You Need Admin Level Power. 
 
-Contact @Reo_47 For More Info ✅</b>"""
+Contact @Eretro_7 For More Info ✅</b>"""
             await message.reply_text(resp)
             return
 
