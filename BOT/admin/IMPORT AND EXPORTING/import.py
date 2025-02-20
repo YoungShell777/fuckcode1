efa42aa35d315c93d7a8e984e096ec9916f33d8b
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-20 14:15:14 -0400

    Update import.py

--- BOT/admin/IMPORT AND EXPORTING/import.py
+++ BOT/admin/IMPORT AND EXPORTING/import.py
@@ -20,7 +20,7 @@ async def stats(Client, message):
         OWNER_ID = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["OWNER_ID"]
         if user_id not in OWNER_ID:
             resp = """<b>You Don't Have Permission To Use This Command.    
-Contact Bot Owner @Reo_47 !</b>"""
+Contact Bot Owner @Eretro_7!</b>"""
             msg1 = await message.reply_text(resp, message.id)
         else:
             try:
